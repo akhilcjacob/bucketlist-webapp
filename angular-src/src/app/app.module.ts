819226790df7c00f1beab440e75251a80b0eeeec
Akhil Jacob <akhilcjacob@gmail.com>
2017-11-28 00:39:01 +0000

    Building Front-end

---
+++ angular-src/src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+
+import { AppComponent } from './app.component';
+import { AddListComponent } from './add-list/add-list.component';
+import { ViewListComponent } from './view-list/view-list.component';
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    AddListComponent,
+    ViewListComponent
+  ],
+  imports: [
+    BrowserModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
